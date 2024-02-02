a301fd24e1c7efaf4b98252b2c031943f504d447
TousifProvat <tousifprovat15@gmail.com>
2024-02-03 01:57:42 +0600

    fix: og tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,21 +4,41 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
+        {/* facebook */}
         <meta
           property='og:image'
           content='https://res.cloudinary.com/dklakm8v6/image/upload/v1706900754/portfolio-images/tousif_ahmed_full_stack_developer_tjzst2.png'
         />
-
+        <meta property='og:url' content='https://tousifahmed.vercel.app/' />
+        <meta property='og:type' content='website' />
         <meta
           property='og:title'
           content='Tousif Ahmed | Full-Stack Developer'
         />
-
         <meta property='og:description' content='Turn ideas into reality' />
-
-        <meta property='og:image:width' content='1200' />
-
-        <meta property='og:image:height' content='630' />
+        <meta
+          property='og:image'
+          content='https://res.cloudinary.com/dklakm8v6/image/upload/v1706900754/portfolio-images/tousif_ahmed_full_stack_developer_tjzst2.png'
+        />
+        {/* twitter */}
+        <meta
+          name='twitter:card'
+          content='Full stack developer specialized in MERN stack and React Native'
+        />
+        <meta property='twitter:domain' content='tousifahmed.vercel.app' />
+        <meta
+          property='twitter:url'
+          content='https://tousifahmed.vercel.app/'
+        />
+        <meta
+          name='twitter:title'
+          content='Tousif Ahmed | Full-Stack Developer'
+        />
+        <meta name='twitter:description' content='Turn ideas into reality' />
+        <meta
+          name='twitter:image'
+          content='https://res.cloudinary.com/dklakm8v6/image/upload/v1706900754/portfolio-images/tousif_ahmed_full_stack_developer_tjzst2.png'
+        />
       </Head>
       <body>
         <Main />
